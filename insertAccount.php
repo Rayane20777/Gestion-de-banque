9a92e241a06fa4b31100c4aa69c14789f7fcada1
Rayan <yayaygrz@gmail.com>
2023-11-24 17:23:08 +0100

    one less form

---
+++ insertAccount.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insert Adresse</title>
+</head>
+<body>
+    <form action="insertAccount.php" method="post">
+        <label for="rib">Rib:</label>
+        <input type="text" id="rib" name="rib" required>
+        <br>
+
+        <label for="devise">Devise:</label>
+        <input type="text" id="devise" name="devise" required>
+        <br>
+
+        <label for="balance">Balance:</label>
+        <input type="text" id="balance" name="balance" required>
+        <br>
+
+        <label for="user_id">User_id:</label>
+        <input type="text" id="user_id" name="user_id" required>
+        <br>
+
+        <input type="submit" value="Insert Adresse">
+    </form>
+</body>
+</html>
+
+
+
+<?php
+include 'index.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $rib = $_POST['rib'];
+    $devise = $_POST['devise'];
+    $balance = $_POST['balance'];
+    $user_id = $_POST['user_id'];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "cih_bank";
+
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO account (rib, devise, balance, user_id) VALUES ('$rib', '$devise' ,'$balance' ,'$user_id')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New adresse created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
---
+++ insertTransaction.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Insert Adresse</title>
+</head>
+<body>
+    <form action="insertTransaction.php" method="post">
+        <label for="type">Type:</label>
+        <select name="type" id="type">
+            <option value="credit">credit</option>
+            <option value="debit">debit</option>
+        </select>
+        <br>
+
+        <label for="amount">Amount:</label>
+        <input type="text" id="amount" name="amount" required>
+        <br>
+
+        <label for="account_id">Account_id:</label>
+        <input type="text" id="account_id" name="account_id" required>
+        <br>
+
+        <input type="submit" value="Insert Adresse">
+    </form>
+</body>
+</html>
+
+
+
+<?php
+include 'index.php';
+
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $type = $_POST['type'];
+    $amount = $_POST['amount'];
+    $account_id = $_POST['account_id'];
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "cih_bank";
+
+    $conn = new mysqli($servername, $username, $password, $database);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    $sql = "INSERT INTO transaction (type, amount, account_id) VALUES ('$type', '$amount' ,'$account_id')";
+
+    if ($conn->query($sql) === TRUE) {
+        echo "New adresse created successfully";
+    } else {
+        echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+    $conn->close();
+}
+?>
